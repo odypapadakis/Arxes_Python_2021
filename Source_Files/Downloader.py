967572ccd67358b586e911e56fc979bc5ba4f23e
Odysseas Papadakis <odypapadakis@gmail.com>
2021-05-12 00:07:00 +0300

    Importer
    Leave improting in another module for another time

--- Source_Files/Downloader.py
+++ Source_Files/Downloader.py
@@ -2,27 +2,27 @@
 
 #  https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/tin00175.tsv.gz
 
-import zipfile
+# import zipfile
+# from my_importer import *
 
 
-try:
-    import requests
-except Exception as e:
-    print("Error! importing module!!")
-    print(e)
-    exit(0)
+# module_name = "requests"
+# import requests
+# importer(module_name)
+
+# test = __import__( module_name )
 
 
-Folder_Name = "Statistics_Downloads"
+
+
 url = "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/tin00175.tsv.gz"
-
 try:
-    response = requests.get(url, timeout=15.00)   # Expect a response within 15 seconds
+    response = requests.get(url, timeout=5.00)   # Expect a response within 5 seconds
 except Exception as a_ex:
     print("ERROR! \n Unable to reach Website", url)
+    print("-----------------------------")
     print(a_ex)                         # Return the error type
+    print("-----------------------------")
     exit(0)
 
-
-path_to_zipfile = ""
-path_to_extracted_zipfile = ""
+print("-------------- DONE -----------------------")
