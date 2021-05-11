84478e0115ebbac03d5b37a301deca4f2eb4b844
Odysseas Papadakis <odypapadakis@gmail.com>
2021-05-12 00:58:49 +0300

    Tkinter small windows

--- Source_Files/Downloader.py
+++ Source_Files/Downloader.py
@@ -2,15 +2,35 @@
 
 #  https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/tin00175.tsv.gz
 
-# import zipfile
-# from my_importer import *
+import os
+import zipfile
+import requests
+
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox as mb
+from tkinter import *
 
 
-# module_name = "requests"
-# import requests
-# importer(module_name)
+root = Tk()
+root.withdraw()
 
-# test = __import__( module_name )
+canvas1 = tk.Canvas(root, width = 300, height = 300)
+canvas1.pack()
+
+current_directory = os.getcwd()
+dir_change = mb.askquestion("Change directory ?", "Current directory is  " + str(current_directory) )
+
+# print(dir_change)
+
+if(dir_change == "yes") :
+    requested_directory = filedialog.askdirectory()
+    os.chdir(requested_directory)
+
+f= open("guru99.txt","w+")
+for i in range(10):
+     f.write("This is line %d\n" % (i+1))
+f.close()
 
 
 
@@ -20,9 +40,7 @@ try:
     response = requests.get(url, timeout=5.00)   # Expect a response within 5 seconds
 except Exception as a_ex:
     print("ERROR! \n Unable to reach Website", url)
-    print("-----------------------------")
     print(a_ex)                         # Return the error type
-    print("-----------------------------")
     exit(0)
 
 print("-------------- DONE -----------------------")
