04fb3242db24aa1232ece0c1310a442536d7cb65
Odysseas Papadakis <odypapadakis@gmail.com>
2021-05-11 23:26:03 +0300

    Created subprograms

--- Source_Files/Downloader.py
+++ Source_Files/Downloader.py
@@ -1,2 +1,28 @@
 # This file contains the code for downloading the csv files.
 
+#  https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/tin00175.tsv.gz
+
+import zipfile
+
+
+try:
+    import requests
+except Exception as e:
+    print("Error! importing module!!")
+    print(e)
+    exit(0)
+
+
+Folder_Name = "Statistics_Downloads"
+url = "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/tin00175.tsv.gz"
+
+try:
+    response = requests.get(url, timeout=15.00)   # Expect a response within 15 seconds
+except Exception as a_ex:
+    print("ERROR! \n Unable to reach Website", url)
+    print(a_ex)                         # Return the error type
+    exit(0)
+
+
+path_to_zipfile = ""
+path_to_extracted_zipfile = ""
---
+++ Source_Files/Imports.py
@@ -0,0 +1,12 @@
+def asdf (salamaki):
+import subprocess
+import sys
+
+# try:
+#     import pandas as pd
+# except ImportError:
+#     subprocess.check_call([sys.executable, "-m", "pip", "install", module_name])
+# finally:
+#     import module_name as pd
+
+print("aa-->",salamaki)
