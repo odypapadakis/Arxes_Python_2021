6dd589f0e027f9f33fd3b592606e2af550bd2288
Odysseas Papadakis <odypapadakis@gmail.com>
2021-05-15 01:34:27 +0300

    data processor progress

--- Source_Files/Main.py
+++ Source_Files/Main.py
@@ -1,6 +1,7 @@
 from directory_selection import *
 from downloader import *
 from data_processor import *
+from data_processor_pandas import *
 from db_store import *
 from charts import *
 
@@ -33,18 +34,19 @@ root.geometry("600x300+650+400")  # Width x Height + Padding left + Padding top
 
 
 
-# Ask for a location to download the data
-directory_change()
+# # Ask for a location to download the data
+# directory_change()
+#
+# dl_files = []
+# #  Feed the downloader with: 1)the url for each file 2)a string to append to each file
+# for i in range(len(URL_list)):
+#     dl_files.append (downloader(URL_list[i][0],URL_list[i][1]))
+#
+#
+# print("-->",dl_files)
 
-dl_files = []
-#  Feed the downloader with: 1)the url for each file 2)a string to append to each file
-for i in range(len(URL_list)):
-    dl_files.append (downloader(URL_list[i][0],URL_list[i][1]))
-
-
-print("-->",dl_files)
-
-# data_processor()
+dl_files_debug = ['Data_Nights.tsv', 'Data_Arrivals.tsv']
+data_processor_pandas(dl_files_debug[0])
 
 # db_stuff()
 
---
+++ Source_Files/data_processor_pandas.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import re
+
+def data_processor_pandas(filename):
+    print("Data processor working on: " + filename)
+    # df = pd.read_csv('filename.csv', sep='\t')
+
+    df = pd.read_table(filename)
+    print(df)
+
+    # df1 = df[df['c_resid,unit,nace_r2,geo\\time'].str.contains('ES' or  'EL', na=False, regex = True) ]
+    # print(df1)
+    print("AAAAAAAAAAA")
+
+    # df = df[df['c_resid,unit,nace_r2,geo\\time'].str.contains(pat = 'ES|EL', regex = True) ]
+
+    df = df.loc[df['c_resid,unit,nace_r2,geo\\time'].str.contains(pat = 'ES|EL', regex = True) ]
+
+    print(df)
+    print(df.columns)
+
+
+    # df =
+    # print("BBBBBBBBB")
+    # df1.drop(list(df1.filter(regex = 'not(2016|2017|2018|2019)')), axis = 1, inplace = True)
+    # df2 = df1.drop(['2018'],axis=1)
+    # column_list = [0,1,2,3]
+
+
+    # df2 =  df2.drop(df2.columns[column_list], axis = 1)
+    # df = df.columns[df.columns.str.contains('2019|2018|2017|2016',axis = 1)]
+    # print(df)
