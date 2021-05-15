91ab66ab0faedb3ecd7ded7105d2c64fb26eaa7e
Odysseas Papadakis <odypapadakis@gmail.com>
2021-05-16 00:45:36 +0300

    Pandas working

--- Source_Files/pandas_playground.py
+++ Source_Files/pandas_playground.py
@@ -11,14 +11,22 @@ print(df)
 
 print("---------------------------------------------")
 
-Selected_countries_RE = "(EL|ES|AT)"
+selected_countries_RE = "EL|ES|AT\Z"
 
+#In the dataframe df, return true if the RE:Selected_countries_RE is true
+mask = (df['c_resid,unit,nace_r2,geo\\time'].str.contains(selected_countries_RE,regex = True))
 
-mask = (df['c_resid,unit,nace_r2,geo\\time'].str.contains(Selected_countries_RE))
-
-print("Mask is: \n",mask)
+# Apply the mask to the dataframe to filter
 df = df[mask]
-
+print(df)
+selected_years_RE = "2016|2017|2018|2019"
+# mask = df[selected_years_RE]
+# df.drop(selected_years_RE, inplace=True, axis=1)
+# df = df[df.columns.drop(list(df.filter(regex=selected_years_RE)))]
+# df.find(list(df.filter(regex = selected_years_RE)), axis = 1, inplace = True)
+print("++++++++++++++++++++++++++++++++++++++++++++++")
+# df = df[[2018,2019]]
+df = df.filter(regex=selected_years_RE, axis=1)
 print(df)
 
 
